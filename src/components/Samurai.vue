e6ae3cc1a65146e0ea7339a2ad02d42c01eedb19
sheshko <sheshkoalexei@gmail.com>
2025-08-16 20:31:20 +0300

    initial commit

---
+++ src/components/Samurai.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="samurai" :class="{ attack: isAttacking, hurt: isHurt }">🧑‍🦲⚔️</div>
+</template>
+
+<script setup>
+defineProps({ isAttacking: Boolean, isHurt: Boolean })
+</script>
+
+<style scoped>
+.samurai {
+  position: absolute;
+  left: 50%; top: 50%;
+  transform: translate(-50%, -50%);
+  font-size: 32px;
+  transition: transform 0.25s ease;
+}
+.samurai.attack { transform: translate(-50%, -50%) rotate(-18deg); }
+.samurai.hurt { animation: shake 0.22s linear 1; }
+@keyframes shake {
+  0% { transform: translate(-50%, -50%) }
+  25% { transform: translate(-52%, -50%) }
+  50% { transform: translate(-48%, -50%) }
+  75% { transform: translate(-51%, -50%) }
+  100% { transform: translate(-50%, -50%) }
+}
+</style>
