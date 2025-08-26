bd5b3c3e1c4347745713258b8a1f29066985d68f
sheshko <sheshkoalexei@gmail.com>
2025-08-26 10:12:06 +0300

    fix deploy

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/typing-game/',
   plugins: [
     vue(),
     vueDevTools(),
