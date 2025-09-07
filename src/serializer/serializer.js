a3d6951c4fcba5161183e10839a1156b960e66c4
sheshko <sheshkoalexei@gmail.com>
2025-09-07 17:06:38 +0300

    fix: serialization

--- src/serializer/serializer.js
+++ src/serializer/serializer.js
@@ -7,6 +7,16 @@ export function gameStringify(storeState) {
         projectiles: storeState.projectiles,
         effects: storeState.effects,
         entities: [storeState.player, ...storeState.enemies, ...storeState.projectiles],
+        stats: storeState.stats,
+        score: storeState.score,
+        isMultiplayer: storeState.isMultiplayer,
+        isReady: storeState.isReady,
+        opponentIsReady: storeState.opponentIsReady,
+        multiplayerBestScore: storeState.multiplayerBestScore,
+        opponentBestScore: storeState.opponentBestScore,
+        opponentCurrentScore: storeState.opponentCurrentScore,
+        room: storeState.room,
+        connectionStatus: storeState.connectionStatus,
         leftActiveAim: () => {}
     });
 
